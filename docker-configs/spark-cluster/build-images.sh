67619afa0db581d3d764d4ee727c4718bd5e53bb
Harshit Bhatt <harshit.hartron@gmail.com>
2019-04-17 09:47:18 +0530

    path resolver

--- docker-configs/spark-cluster/build-images.sh
+++ docker-configs/spark-cluster/build-images.sh
@@ -1,6 +1,6 @@
 set -e
 
-docker build -t spark-base:2.3.1 ./docker/base
-docker build -t spark-master:2.3.1 ./docker/spark-master
-docker build -t spark-worker:2.3.1 ./docker/spark-worker
-docker build -t spark-submit:2.3.1 ./docker/spark-submit
+docker build -t spark-base:2.3.1 ./spark-base
+docker build -t spark-master:2.3.1 ./spark-master
+docker build -t spark-worker:2.3.1 ./spark-worker
+docker build -t spark-submit:2.3.1 ./spark-submit
